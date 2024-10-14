f5caa800840c396240adb28b4320f8f80a6ff53c
spart <spart@LAPTOP-EAHPQA0P>
2024-10-14 22:13:37 +0400

    All my Python exercises exist here

--- first_tasks/first_ex.py
+++ first_tasks/first_ex.py
@@ -1,12 +1,12 @@
-    sum =0
-    while True:
+sum =0
+while True:
             choice =int(input("Please choose 1 to add numbers or 2 to stop  "))
             if choice == 1:
                 num = float(input("Please input a number  "))
                 sum += num
                 print(f"Sum is:  {sum}")
             elif choice == 2:
-            print(f"Final sum is:  {sum}")
-            break
-        else:
-            print("invalid choice. Please choose 1 or 2.  ")
+                print(f"Final sum is:  {sum}")
+                break
+            else:
+                print("invalid choice. Please choose 1 or 2.  ")
